3535b1dcaff38c710bf4c2f37840bff341be1616
Justyna <121856593+JustynaJac@users.noreply.github.com>
2023-07-06 20:48:49 +0200

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,7 +8,9 @@ class LoginPage(BasePage):
     remind_password_hyperlink_xpath = "//*[@id='__next']/form/div/div[1]/a"
     language_select_xpath = "div[role='button'][aria-haspopup='listbox']"
     logo_image_xpath = "///*[text()='Scouts Panel']"
-
+    email_field_xpath = "///*[text()='E-mail']"
+    back_to_sign_in_xpath = "///*[text()='Back to sign in']"
+    send_button__xpath = "///*[text()='Send']"
 
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
