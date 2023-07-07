f8ba59ca01fe7869567079879c857bae97fd3343
Justyna <121856593+JustynaJac@users.noreply.github.com>
2023-07-07 19:21:52 +0200

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -13,7 +13,6 @@ class Dashboard(BasePage):
     button_main_page_xpath = "//*[text()='Strona główna']"
     button_players_xpath = "//*[text()='Players']"
     language_select_xpath = "//*[@id='__next']/div[1]/div/div/div/ul[2]/div[1]/div[2]/span"
-selectors
     sign_out_xpath = "//*[@id ='__next']/div[1]/div/div/div/ul[2]/iv[2]/div[1]"
     sign_out_button_xpath = "//span[text()='Wyloguj']"
     logo_futbol_xpath = "//div[@class='MuiCardMedia-root jss8']"
@@ -28,11 +27,3 @@ selectors
     def title_of_page(self):
         time.sleep(4)
         assert self.get_page_title(self.dashboard_url) == self.expected_title
-
-    sign_out_xpath = "//*[@id ='__next']/div[1]/div/div/div/ul[2]/ iv[2]/div[1]"
-    sign_out_button_xpath = "// span[text() = 'Wyloguj']"
-    logo_futbol_xpath = "//div[ @class ='MuiCardMedia-root jss8']"
-    button_devteam_xpath =  "//*[text()='Dev team contact']"
-    header_activity_xpath = "//h2[contains( @class,'MuiTypography-root')andcontains(@class,'MuiTypography-h5')and contains(@ class,'MuiTypography-gutterBottom')andtext()='Aktywnosć']"
-    button_addplayer_xpath = "//a[@href='/pl/players/add']/button"
- main
