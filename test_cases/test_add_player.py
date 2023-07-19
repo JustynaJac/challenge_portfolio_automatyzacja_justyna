b8c3fb5680443784319bf0d1885c6e06b750394b
Justyna <121856593+JustynaJac@users.noreply.github.com>
2023-07-19 19:45:29 +0200

    Update test_add_player.py

--- test_cases/test_add_player.py
+++ test_cases/test_add_player.py
@@ -6,11 +6,9 @@ from selenium.webdriver.chrome.service import Service
 from utils.settings import IMPLICITLY_WAIT
 from pages.login_page import LoginPage
 from pages.dashboard import Dashboard
-from pages.add_a_player import AddingMatch
+from pages.add_a_player import AddAPlayer
 
-
-class TestAddPlayer(unittest.TestCase):
-
+class TestAddAPlayer(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         driver_path = "C:/Users/justy/Documents/GitHub/challenge_portfolio_automatyzacja_justyna/drivers/"
@@ -21,19 +19,34 @@ class TestAddPlayer(unittest.TestCase):
         cls.driver.fullscreen_window()
         cls.driver.implicitly_wait(IMPLICITLY_WAIT)
 
-    def test_add_player_page(self):
+    def test_add_a_player_to_database(self):
         user_login = LoginPage(self.driver)
         user_login.type_in_email('user09@getnada.com')
         user_login.type_in_password('Test-1234')
         user_login.click_sign_in_button()
-        time.sleep(5)
+        dashboard_page = Dashboard(self.driver)
+        dashboard_page.click_add_a_player_button()
+        add_player_page = AddAPlayer(self.driver)
+        add_player_page.type_in_email()
+        add_player_page.type_in_name()
+        add_player_page.type_in_surname()
+        add_player_page.type_in_phone()
+        add_player_page.type_in_weight()
+        add_player_page.type_in_height()
+        add_player_page.type_in_age()
+        add_player_page.select_leg()
+        add_player_page.type_in_club()
+        add_player_page.type_in_level()
+        add_player_page.type_in_main_position()
+        add_player_page.type_in_second_position()
+        add_player_page.select_district()
+        add_player_page.type_in_achievements()
+        add_player_page.click_submit_button()
 
-        dashboard = Dashboard(self.driver)
-        dashboard.click_button_add_player()
 
-        adding_match = AddingMatch(self.driver)
-        adding_match.assert_add_player_page_loaded()
-
+    @classmethod
+    def tearDown(self):
+        self.driver.quit()
     @classmethod
     def tearDownClass(cls):
         cls.driver.quit()
